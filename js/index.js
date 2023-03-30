7e35d7bf30a38eb5e555575bae6bb8e1f110e745
Rupesh-Darimisetti <49586865+Rupesh-Darimisetti@users.noreply.github.com>
2023-03-30 18:25:43 +0530

    #1 navbar with responsive design
    created header with responsive design with both normal and dropdown elements in the navbar header section

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// NavBar drop down menu code
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}

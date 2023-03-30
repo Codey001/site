fdf7d2da3b2aa6f270f7194f0a07b6062b3a291b
Abhinav <abhinavrajputaes@gmail.com>
2023-03-30 22:24:19 +0530

    modified the navBar to look more attractive, added logo and the profile icon

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 // NavBar drop down menu code
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+hamburger.addEventListener("click", mobileMenu);
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 }

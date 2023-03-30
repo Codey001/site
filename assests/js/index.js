ff9b327634c19fb29eb10186626f6d3d283104d9
Rupesh-Darimisetti <49586865+Rupesh-Darimisetti@users.noreply.github.com>
2023-03-30 20:48:20 +0530

    modify the folder structure to organise the code neatly

---
+++ assests/js/index.js
@@ -0,0 +1,9 @@
+// NavBar drop down menu code
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
